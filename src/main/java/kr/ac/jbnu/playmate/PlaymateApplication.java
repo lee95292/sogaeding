01ed3577f7d83f2b0c04c4006d21ca5af15a7c3a
Djunnni <withdong854@naver.com>
2019-08-18 02:12:42 +0900

    기초작업중

--- src/main/java/kr/ac/jbnu/playmate/PlaymateApplication.java
+++ src/main/java/kr/ac/jbnu/playmate/PlaymateApplication.java
@@ -1,13 +1,32 @@
 package kr.ac.jbnu.playmate;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//DB 설정 전에 일부러 강제 설정 => 설정후 삭제 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@ComponentScan
 @SpringBootApplication
+
+//DB 설정하지 않은 상태 => 설정 후 삭제 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class PlaymateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlaymateApplication.class, args);
 	}
 
+	@Bean
+	public InternalResourceViewResolver setupViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		
+		resolver.setPrefix("/WEB-INF/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
 }
---
+++ src/main/webapp/WEB-INF/main.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PlayMate 학습관리시스템</title>
+</head>
+<body>
+<img src="resources/imgs/background/background-01.jpg">
+메인화면 제작중입니다.
+
+</body>
+</html>
