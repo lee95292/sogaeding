80e6e9cfaf758ee78d29295da12ab643777b2f47
lee95292 <lee95292@naver.com>
2019-08-17 15:51:12 +0900

    2019-08-17 %time % lee95292

---
+++ src/main/java/kr/ac/jbnu/playmate/PlaymateApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.playmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaymateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaymateApplication.class, args);
+	}
+
+}
