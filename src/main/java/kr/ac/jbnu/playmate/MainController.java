56da874c3ac5b0c4db877c5b5cff3a3d5125ef20
Djunnni <withdong854@naver.com>
2019-08-18 17:14:31 +0900

    login view 구

--- src/main/java/kr/ac/jbnu/playmate/MainController.java
+++ src/main/java/kr/ac/jbnu/playmate/MainController.java
@@ -1,13 +1,17 @@
 package kr.ac.jbnu.playmate;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
-	public String main() {
+	public String main(Model model) {
 		return "main";
 	}
 }
